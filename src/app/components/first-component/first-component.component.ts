b619bfa5dc98c0ee4d89255ba430fb4c434f1877
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-11-07 11:45:52 -0300

    Transfer data between app and components

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent implements OnInit {
+  name: string = 'Ivan Henriques';
+  age: number = 48;
+  languages = ['C#', 'Python', 'Javascript'];
+  car = { factory: 'Honda', model: 'Fit', year: 2019 };
 
   constructor() { }
 
